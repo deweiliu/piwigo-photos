e8cb563a55e18ae24b54e34141f1bb3ea2e78ddc
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2021-12-14 00:05:54 +0000

    Update Dockerfile

--- storage/command.sh
+++ storage/command.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name PiwigoPhotosStorage --template-body file://cloudformation.yml --tags Key=service,Value=piwigo-photos;
+aws cloudformation create-stack --stack-name OtherStorage --template-body file://cloudformation.yml --tags Key=service,Value=piwigo-photos;
