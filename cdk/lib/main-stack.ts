6ae805efb5bf156eec1b27b435b19809b95d8e89
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2021-12-19 03:26:51 +0000

    Update container momory size

--- cdk/lib/main-stack.ts
+++ cdk/lib/main-stack.ts
@@ -99,7 +99,7 @@ export class CdkStack extends cdk.Stack {
     const container = taskDefinition.addContainer('Container', {
       image: ecs.ContainerImage.fromRegistry(get.dockerImage),
       containerName: `${get.appName}-container`,
-      memoryReservationMiB: 256,
+      memoryReservationMiB: 32,
       portMappings: [{ containerPort: 80, hostPort: get.hostPort, protocol: ecs.Protocol.TCP }],
       logging: new ecs.AwsLogDriver({ streamPrefix: get.appName }),
     });
